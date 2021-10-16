e9d2b82fcdef8fb749ab8e3f70712e7ec6d48ec5
Dan Chamorro <i@danchamorro.com>
2021-10-16 10:45:44 -0400

    updated code to account for wrong input

--- RockPaperScissors/rock-paper-scissors.py
+++ RockPaperScissors/rock-paper-scissors.py
@@ -33,12 +33,16 @@ print('Welcome to Rock, Paper, Scissors.')
 
 rpc = ["Rock", "Paper", "Scissors"]
 rpc_image = [rock, paper, scissors]
-user_choice = int(input(
-    "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors \n"))
+try:
+    user_choice = int(input(
+        "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors \n"))
+except:
+    user_choice = -1
 cpu_choice = random.randint(0, len(rpc)-1)
 
-
-if user_choice == 0 and cpu_choice == 2:
+if user_choice >= 3 or user_choice < 0:
+    print("You did't type a valid number, you lose!")
+elif user_choice == 0 and cpu_choice == 2:
     print(rpc_image[user_choice])
     print("The Computer Chose: \n", rpc_image[cpu_choice])
     print(f"You choose: {rpc[user_choice]}. You win!")
