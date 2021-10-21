7584c3b334068dd196bb13ae984a8fd264f53cd8
Dan Chamorro <i@danchamorro.com>
2021-10-21 12:47:51 -0400

    completed calculator app 100DOC

--- Calculator/calculator_app.py
+++ Calculator/calculator_app.py
@@ -18,3 +18,45 @@ def multiply(n1, n2):
 # Divide
 def divide(n1, n2):
     return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+
+def calculator():
+    """[summary]
+    Do basic math functions with the ability to chain math functions on the previous result
+    """
+    num1 = int(input("What is the first number?: "))
+    for operation in operations.keys():
+        print(operation)
+    operation_symbol = input("Pick an operation from the line above: ")
+    num2 = int(input("What is the second number?: "))
+    calculation_function = operations[operation_symbol]
+    first_answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+
+    contin = True
+
+    while contin:
+        contin_calculations = input(
+            f"Type 'y' to continue calculating with {first_answer}, or type 'n' to start a new calculation, or type 'e' to exit: ")
+        if contin_calculations == "y":
+            operation_symbol = input("Pick an another operation: ")
+            num3 = int(input("What is the second number?: "))
+            calculation_function = operations[operation_symbol]
+            second_answer = calculation_function(first_answer, num3)
+            print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+        elif contin_calculations == "n":
+            contin = False
+            calculator()
+        else:
+            contin = False
+
+
+calculator()
