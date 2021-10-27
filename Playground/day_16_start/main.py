4a07165b98e36f57fcd48be789a5ce18577f462f
Dan Chamorro <i@danchamorro.com>
2021-10-27 16:52:22 -0400

    playing with prettytable

---
+++ Playground/day_16_start/main.py
@@ -0,0 +1,20 @@
+# from turtle import Turtle, Screen
+
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("green")
+
+# timmy.forward(100)
+
+# my_screen = Screen()
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirlte", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+print(table)
