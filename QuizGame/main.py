1e09a1f079a6aa415013d3ac2985d8baef18e5c7
Dan Chamorro <i@danchamorro.com>
2021-10-29 22:13:16 -0400

    added next_question method

--- QuizGame/main.py
+++ QuizGame/main.py
@@ -1,10 +1,14 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
 for question in question_data:
-    for text, answer in question.items():
-        question_bank.append(Question(text, answer))
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
-print(question_bank[0].answer)
+quiz = QuizBrain(question_bank)
+quiz.next_question()
