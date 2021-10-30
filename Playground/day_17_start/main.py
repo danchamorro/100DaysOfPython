4e77a55e336645e7c05f88f1033f32b41655a8a4
Dan Chamorro <i@danchamorro.com>
2021-10-29 21:21:18 -0400

    exit playground to work on QuizGame

--- Playground/day_17_start/main.py
+++ Playground/day_17_start/main.py
@@ -4,10 +4,18 @@ class User:
         self.username = username
         # This is a default value and doesn't need to be added an argument.
         self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
 
 user_1 = User("001", "angela")
 user_2 = User("002", "jack")
 
+# This is user_1 following user_2, based on the follow method in the User Class.
+user_1.follow(user_2)
 
-print(user_1.username)
+print(user_1.followers)  # This is 0 because no one is follwing user_1
+print(user_1.following)  # This is 1 because we set user_1 to follow user_2
