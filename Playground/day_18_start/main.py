95e9055ce92979ee47df3dfba4a8402fd8320a6b
Dan Chamorro <i@danchamorro.com>
2021-11-06 15:52:45 -0400

    draw random with different colors.

--- Playground/day_18_start/main.py
+++ Playground/day_18_start/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+import random
 
 
 timmy_the_turtle = Turtle()
@@ -8,14 +9,28 @@ timmy_the_turtle.color("blue")
 
 # create a function to draw a line with dashes using penup and pendown
 def draw_line_with_dashes(length, number_of_dashes):
-    for i in range(number_of_dashes):
+    for _ in range(number_of_dashes):
         timmy_the_turtle.forward(length)
         timmy_the_turtle.penup()
         timmy_the_turtle.forward(length)
         timmy_the_turtle.pendown()
 
 
-draw_line_with_dashes(10, 10)
+#draw_line_with_dashes(10, 10)
+
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed",
+           "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]  # list of colours
+directions = [0, 90, 180, 270]  # list of directions
+timmy_the_turtle.pensize(15)  # set the pen size
+timmy_the_turtle.speed("fastest")  # set the speed
+
+for _ in range(200):  # loop 200 times
+    # choose a random colour from the list
+    timmy_the_turtle.color(random.choice(colours))
+    timmy_the_turtle.forward(30)  # move forward 30 pixels
+    # set the heading to a random direction from the list
+    timmy_the_turtle.setheading(random.choice(directions))
 
 
 screen = Screen()
