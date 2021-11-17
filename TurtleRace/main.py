8c6e5182d854079b2d7fee1dc9041576dfee5f99
Dan Chamorro <i@danchamorro.com>
2021-11-17 18:26:15 -0500

    created 6 tutrles with different colors using forloop

--- TurtleRace/main.py
+++ TurtleRace/main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, color
 
 
 screen = Screen()
@@ -8,13 +8,17 @@ screen.setup(width=500, height=400, startx=None, starty=None)
 # Create a popup window
 user_bet = screen.textinput(title="Make your bet",
                             prompt="What color turtle will win? ")
-print(user_bet)
+colors = ["red", "green", "blue", "yellow", "orange", "purple"]
+y_pos = [-70, -40, -10, 20, 50, 80]
 
-
-tim = Turtle(shape="turtle")
-# Set the turtle starting position.
-tim.penup()
-tim.goto(x=-230, y=100)
+# Create 6 turtles with different colors and different y positions
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    # Set the turtle starting position.
+    tim.penup()
+    tim.goto(x=-230, y=y_pos[turtle_index])
+    # Set the turtle color
+    tim.color(colors[turtle_index])
 
 
 screen.exitonclick()
