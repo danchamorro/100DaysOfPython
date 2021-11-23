6855b50bf60b0cb3402360e45f0971c00aa1fc86
Dan Chamorro <i@danchamorro.com>
2021-11-22 20:36:58 -0500

    beggining of snake game

---
+++ SnakeGame/main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+# Snake Game
+
+# Create a screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+# Create the snake body consting of three squares 20X20 and color is white
+snake = Turtle()    # Create a turtle
+
+
+# Exit on click
+screen.exitonclick()
